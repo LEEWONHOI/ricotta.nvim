ac7c385e41d4167ce7e7e9e3031b32279db118b9
이원회 <wh.lee@piolink.com>
2024-03-18 13:41:21 +0900

    R #1 간단 커스텀 설정 및 lsp 옵션 on
    - 커스텀
    -- 폰트옵션 on
    -- tab으로 나오는 목록을 엔터로 선택하도록 변경 (기존 컨트롤 r)
    - lsp
    -- python, c, robot, go lsp 옵션 on

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -57,7 +57,10 @@ return {
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+          -- 커스텀
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
